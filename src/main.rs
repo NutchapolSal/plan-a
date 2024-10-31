c594e05b07d3528ff6ec306a365215ff79436909
NutchapolSal <nutchapol.sal@gmail.com>
2024-10-31 12:18:15 +0700

    remove unused code in main

--- src/main.rs
+++ src/main.rs
@@ -48,16 +48,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     })?;
     let ocr = Arc::new(ocr);
 
-    // let test_img = ImageReader::open("./userdata/plans/azurlane/assets/settings-header.png")?
-    //     .decode()?
-    //     .to_rgb8();
-    // let test_img = ImageSource::from_bytes(test_img.as_raw(), test_img.dimensions())?;
-    // let test_img = ocr.prepare_input(test_img)?;
-    // let test_output = ocr.get_text(&test_img)?;
-    // println!("{:?}", test_output);
-
-    // return Ok(());
-
     println!("Hello, world!");
 
     let plan_wd = PathBuf::from(&userdata_path.join("plans/azurlane")); // TODO
@@ -73,144 +63,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     return Ok(());
 
-    // globals.set(
-    //     "sleep",
-    //     lua.create_function(|_, seconds: f64| {
-    //         sleep(Duration::from_secs_f64(seconds));
-    //         Ok(())
-    //     })?,
-    // )?;
-
-    // lua.load(
-    //     r#"
-    //     print("Hello from Lua!")
-    //     sleep(1)
-    // "#,
-    // )
-    // .exec()?;
-    // println!("rust code");
-    // lua.load(
-    //     r#"
-    //     print("More Lua!")
-    //     sleep(1)
-    // "#,
-    // )
-    // .exec()?;
-    // println!("more rust code");
-
-    // {
-    //     let mut rust_val = 0;
-    //     println!("{}", rust_val);
-    //     lua.scope(|scope| {
-    //         lua.globals().set(
-    //             "sketchy",
-    //             scope.create_function_mut(|_, val: i64| {
-    //                 rust_val = val;
-    //                 Ok(())
-    //             })?,
-    //         )?;
-
-    //         lua.load(
-    //             r#"
-    //             sketchy(5)
-    //         "#,
-    //         )
-    //         .exec()
-    //     })?;
-
-    //     println!("{}", rust_val);
-    // }
-
-    // return Ok(());
 
     println!("{}", Local::now().format("%Y-%m-%d %H:%M:%S"));
-    // let mut i = 0;
-    let mut state = "start";
-    // loop {
-    //     'inner: {
-    //         // println!("{} {}", Local::now().format("%Y-%m-%d %H:%M:%S"), state);
-    //         // let res = device.framebuffer_inner();
-    //         // let image = match res {
-    //         //     Err(err) => {
-    //         //         println!("{:?}", err);
-    //         //         break 'inner;
-    //         //     }
-    //         //     Ok(image) => image,
-    //         // };
-    //         // image.save(format!(
-    //         //     "./temp/{}.png",
-    //         //     Local::now().format("%Y-%m-%d %H-%M-%S")
-    //         // ))?;
-
-    //         // let image: RgbaImage = downgrade_image(image);
-    //         // let image = DynamicImage::from(image).to_luma32f();
-
-    //         // let state_data = plan.screens.get(state).unwrap();
-    //         // if let Some(next) = state_data.next.as_ref() {
-    //         //     let mut matches: Vec<_> = next
-    //         //         .iter()
-    //         //         .map(|n| {
-    //         //             let next_state_data = plan.screens.get(n).unwrap();
-    //         //             let path = PathBuf::from("./userdata/plans/azurlane/")
-    //         //                 .join(next_state_data.ident.as_ref().unwrap());
-    //         //             let template = ImageReader::open(path)?.decode()?.to_luma8();
-    //         //             let template = DynamicImage::from(template).to_luma32f();
-
-    //         //             let res = match_template(
-    //         //                 &image,
-    //         //                 &template,
-    //         //                 MatchTemplateMethod::SumOfSquaredDifferences,
-    //         //             );
-    //         //             Ok::<_, Box<dyn Error>>((n, find_extremes(&res)))
-    //         //         })
-    //         //         .filter_map(|res| match res {
-    //         //             Ok(res) => Some(res),
-    //         //             Err(err) => {
-    //         //                 println!("{:?}", err);
-    //         //                 None
-    //         //             }
-    //         //         })
-    //         //         .filter(|(_, ext_a)| ext_a.min_value < 1000.0)
-    //         //         .collect();
-
-    //         //     matches.sort_by(|(_, ext_a1), (_, ext_a2)| {
-    //         //         ext_a1.min_value.partial_cmp(&ext_a2.min_value).unwrap()
-    //         //     });
-
-    //         //     if let Some((next, _)) = matches.first() {
-    //         //         state = next;
-    //         //         break 'inner;
-    //         //     }
-    //         // }
-
-    //         // if let Some(to) = plan.screens.get(state).unwrap().to.as_ref() {
-    //         //     let to = to.first().unwrap();
-    //         //     for act in &to.act {
-    //         //         match act {
-    //         //             def::Actions::Tap(pos) => {
-    //         //                 device.tap(pos[0], pos[1])?;
-    //         //             }
-    //         //         }
-    //         //     }
-    //         //     state = &to.state;
-    //         //     break 'inner;
-    //         // }
-    //         // panic!("state should have next or to");
-    //     }
-
-    //     if state == "end" {
-    //         break;
-    //     }
-
-    //     // i += 1;
-    //     sleep(Duration::from_secs(15));
-    // }
-
-    // let mut echo_hello = Command::new("adb");
-    // // echo_hello.arg("-h");
-    // let hello_1 = echo_hello.output().expect("failed to execute process");
-    // // let hello_2 = echo_hello.output().expect("failed to execute process");
-    // println!("{:?}", hello_1);
 
     // let mdns = ServiceDaemon::new().expect("should be able to create mdns service daemon");
     // let recv = mdns
@@ -239,16 +93,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     // std::thread::sleep(std::time::Duration::from_secs(10));
     // mdns.shutdown().unwrap();
 
-    // template_matching_example(
-    //     "./assets/weekly missions claimall.png",
-    //     "./assets/claimall button.png",
-    //     "./temp/a",
-    // )?;
-    // template_matching_example(
-    //     "./assets/weekly missions claim.png",
-    //     "./assets/claimall button.png",
-    //     "./temp/b",
-    // )?;
     Ok(())
 }
 
@@ -337,29 +181,3 @@ fn try_connect_to_device(
     }
 }
 
-fn template_matching_example(
-    image_path: &str,
-    template_path: &str,
-    output_path: &str,
-) -> Result<(), Box<dyn Error>> {
-    let image = ImageReader::open(image_path)?.decode()?.to_luma8();
-    let image = DynamicImage::from(image).to_luma32f();
-    let template = ImageReader::open(template_path)?.decode()?.to_luma8();
-    let template = DynamicImage::from(template).to_luma32f();
-    let out = match_template(
-        &image,
-        &template,
-        MatchTemplateMethod::SumOfSquaredDifferences,
-    );
-    let ext_a = find_extremes(&out);
-    println!("{:?}", ext_a);
-    let out = convert_luma_f32_to_u8(
-        ImageBuffer::<Luma<f32>, Vec<f32>>::from_raw(out.width, out.height, out.data.to_vec())
-            .unwrap(),
-        ext_a.max_value,
-    );
-    DynamicImage::from(out)
-        .to_luma8()
-        .save(format!("{output_path} out.png"))?;
-    Ok(())
-}
